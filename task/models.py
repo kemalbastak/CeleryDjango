f745709d6b09f2ae3a595ac2fdde4d1e76c6d741
kemalbastak <bastakkemal@gmail.com>
2021-11-10 17:38:43 +0300

    first commit

---
+++ task/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+
+class Tasks(models.Model):
+    assignee = models.CharField(verbose_name=_("Atayan Kişi"), max_length=100)
+    task_name = models.CharField(verbose_name=_("Görev İsmi"), max_length=100)
+    priority = models.CharField(verbose_name=_("Öncelik"), max_length=100, choices=(("Acil", 1),("Önemli", 2),("Az Önemli", 3)))
+    status = models.CharField(verbose_name=_("Durum"), max_length=100, choices=(("Bekleniyor", 1),("İşleme Alındı", 2),("Revize Verildi", 3)))
+    due_date = models.DateTimeField(auto_now_add=False, verbose_name=_("Termin Tarihi"))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Veriliş Tarihi"))
+
+    def __str__(self) -> str:
+        return f"{self.task_name}"
+
+    class Meta:
+        verbose_name = _('Görevler')
+        verbose_name_plural = _('Görevler')
