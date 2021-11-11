816a0c111830ec86c1085fe1ad8b84a7f0aa745d
kemalbastak <pisiko007@>
2021-11-11 17:52:41 +0300

    setting.py fixed

--- core/settings.py
+++ core/settings.py
@@ -144,6 +144,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='bastakkemal@gmail.com'
-EMAIL_HOST_PASSWORD = "pisiko007"
+EMAIL_HOST_USER =
+EMAIL_HOST_PASSWORD =
 
