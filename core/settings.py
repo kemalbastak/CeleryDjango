d4fb18eafa959b7696ed46cd95b4c2a5e50703cc
Kemal Cihan Baştak <bastakkemal@gmail.com>
2021-11-13 11:07:46 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -149,6 +149,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bastakkemal@gmail.com'
-EMAIL_HOST_PASSWORD = 'pisiko007'
+EMAIL_HOST_USER = 'your@email.com'
+EMAIL_HOST_PASSWORD = 'your_password'
 
